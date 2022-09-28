0cf081c89d03c813fcb14ad39fa17bf459f940fb
Vivek Yadav <vivek.yadav472@gmail.com>
2022-09-28 10:41:22 +0530

    Pew pew, Player and Shooting :)

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use crossterm::cursor::{Hide, Show};
 use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use crossterm::{event, terminal, ExecutableCommand};
-use invaders::frame::new_frame;
+use invaders::frame::{new_frame, Drawable};
+use invaders::player::Player;
 use invaders::{frame, render};
 use rusty_audio::Audio;
 use std::error::Error;
 use std::io;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use std::sync::mpsc::channel;
 use std::thread;
@@ -45,11 +46,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     //Game loop
+    let mut player = Player::new();
+    let mut instant = Instant::now();
     'gameloop: loop {
-        let curr_frame = new_frame();
+        let delta = instant.elapsed();
+        instant = Instant::now();
+        let mut curr_frame = new_frame();
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
+                    KeyCode::Char(' ') | KeyCode::Enter => {
+                        if player.shoot() {
+                            audio.play("pew");
+                        }
+                    }
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -59,12 +71,17 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         }
 
+        //Updates
+        player.update(delta);
+
+        //Draw & render
+        player.draw(&mut curr_frame);
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
 
+    //Cleanup
     render_handle.join().unwrap();
-
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
